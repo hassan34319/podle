e83d8839e5a97881781c0506c5ee2d9e592e9cf0
hassan34319 <mh08062@st.habib.edu.pk>
2023-12-08 12:11:35 +0500

    Fixing upload samples

--- app/serviceProvider/addGig/(AddGigComponents)/Samples.tsx
+++ app/serviceProvider/addGig/(AddGigComponents)/Samples.tsx
@@ -1,6 +1,6 @@
 import { client } from "@/app/utils/client";
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 type Props = {
   imageUrls: string[];
@@ -9,42 +9,34 @@ type Props = {
 };
 
 function Samples({ imageUrls, setImageUrls, handleSubmit }: Props) {
-  const fileInputRef1 = useRef<HTMLInputElement>(null);
-  const fileInputRef2 = useRef<HTMLInputElement>(null);
-  const fileInputRef3 = useRef<HTMLInputElement>(null);
-  const handleImageUpload = async (
-    event: React.ChangeEvent<HTMLInputElement>,
-    inputIndex: number
-  ) => {
-    const file = event.target.files?.[0];
+  const [selectedImages, setSelectedImages] = useState<File[]>([]);
+  const handleImageUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
 
-    if (file) {
-      const reader = new FileReader();
+    if (files) {
+      const selectedFiles = Array.from(files).slice(0, 3); // Limit selection to three files
+      setSelectedImages(selectedFiles);
 
-      reader.onload = async (e) => {
-        const uploadedImage = e.target?.result as string;
+      const uploadedImageUrls = await Promise.all(
+        selectedFiles.map(async (file) => {
+          // Simulate uploading the image and retrieve its URL
+          const imageUrl = await uploadImageToSanity(file);
+          return imageUrl;
+        })
+      );
 
-        // Upload the image to Sanity and create an asset
-        const imageUrl = await uploadImageToSanity(file);
-
-        // Add the uploaded image URL to the array state
-
-        if (imageUrls.length > 0) {
-          const updatedImageUrls = [...imageUrls];
-          if (updatedImageUrls.length < 3) {
-            updatedImageUrls.push(imageUrl);
-          } else {
-            updatedImageUrls[inputIndex - 1] = imageUrl;
-          }
-
-          setImageUrls(updatedImageUrls.slice(0, 3));
-        } else {
-          setImageUrls([imageUrl]);
-        }
-      };
-
-      reader.readAsDataURL(file);
+      setImageUrls(uploadedImageUrls);
     }
+  };
+
+  const handleImageDelete = (index: number) => {
+    const updatedImages = [...selectedImages];
+    updatedImages.splice(index, 1);
+    setSelectedImages(updatedImages);
+
+    const updatedImageUrls = [...imageUrls];
+    updatedImageUrls.splice(index, 1);
+    setImageUrls(updatedImageUrls);
   };
 
   const uploadImageToSanity = async (file: File) => {
@@ -73,75 +65,32 @@ function Samples({ imageUrls, setImageUrls, handleSubmit }: Props) {
         Showcase your work to your clients.
       </h1>
 
-      <div className="text-sm md:text-base mt-1  md:mt-2 lg:mt-4 w-full xl:w-[50%] rounded-md overflow-hidden h-10 md:h-10 lg:h-12 bg-transparent border-2 xl:rounded-xl border-black border-opacity-[30%] flex justify-end">
-        {imageUrls[0] && (
-          <Image
-            src={imageUrls[0]}
-            alt={imageUrls[0]}
-            className="object-contain"
-            width={100}
-            height={100}
-          />
-        )}
-        <button
-          onClick={() => openFileInput(fileInputRef1)}
-          className="h-full px-2 md:px-4 py-2 border-2 border-black rounded-tr-md rounded-br-md xl:rounded-tr-xl xl:rounded-br-xl  font-bold text-black md:py-4 bg-white flex justify-center items-center text-sm md:text-base"
-        >
-          <input
-            ref={fileInputRef1}
-            type="file"
-            className="hidden"
-            onChange={(e) => handleImageUpload(e, 1)}
-          />
-          Upload Work
-        </button>
+      {/* Single input for multiple images */}
+      <div className="text-sm md:text-base mt-1 md:mt-2 lg:mt-4 w-full xl:w-[50%]">
+        <input
+          type="file"
+          accept="image/*"
+          multiple
+          onChange={handleImageUpload}
+        />
+        {imageUrls.map((imageUrl, index) => (
+          <div key={index} className="relative inline-block mr-2">
+            <Image
+              src={imageUrl}
+              alt={`Uploaded image ${index + 1}`}
+              width={100}
+              height={100}
+            />
+            <button
+              className="absolute top-0 right-0 bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center -mr-2 -mt-2"
+              onClick={() => handleImageDelete(index)}
+            >
+              X
+            </button>
+          </div>
+        ))}
       </div>
-      <div className="text-sm md:text-base mt-1  md:mt-2 lg:mt-4 w-full xl:w-[50%] rounded-md overflow-hidden h-10 md:h-10 lg:h-12 bg-transparent border-2 xl:rounded-xl border-black border-opacity-[30%] flex justify-end">
-        {imageUrls[1] && (
-          <Image
-            src={imageUrls[1]}
-            alt={imageUrls[1]}
-            className="object-contain"
-            width={100}
-            height={100}
-          />
-        )}
-        <button
-          onClick={() => openFileInput(fileInputRef2)}
-          className="h-full px-2 md:px-4 py-2 border-2 border-black rounded-tr-md rounded-br-md xl:rounded-tr-xl xl:rounded-br-xl  font-bold text-black md:py-4 bg-white flex justify-center items-center text-sm md:text-base"
-        >
-          <input
-            ref={fileInputRef2}
-            type="file"
-            className="hidden"
-            onChange={(e) => handleImageUpload(e, 2)}
-          />
-          Upload Work
-        </button>
-      </div>
-      <div className="text-sm md:text-base mt-1  md:mt-2 lg:mt-4 w-full xl:w-[50%] rounded-md overflow-hidden h-10 md:h-10 lg:h-12 bg-transparent border-2 xl:rounded-xl border-black border-opacity-[30%] flex justify-end">
-        {imageUrls[2] && (
-          <Image
-            src={imageUrls[2]}
-            alt={imageUrls[2]}
-            className="object-contain"
-            width={100}
-            height={100}
-          />
-        )}
-        <button
-          onClick={() => openFileInput(fileInputRef3)}
-          className="h-full px-2 md:px-4 py-2 border-2 border-black rounded-tr-md rounded-br-md xl:rounded-tr-xl xl:rounded-br-xl  font-bold text-black md:py-4 bg-white flex justify-center items-center text-sm md:text-base"
-        >
-          <input
-            ref={fileInputRef3}
-            type="file"
-            className="hidden"
-            onChange={(e) => handleImageUpload(e, 3)}
-          />
-          Upload Work
-        </button>
-      </div>
+
       <button
         className={`bg-[#16A235] xl:w-[50%] mt-2 md:mt-4 lg:mt-8 border-white hover:bg-opacity-80 rounded-lg border-[0.5px] font-medium text-white w-full py-4 `}
         onClick={handleSubmit}
